eea1642eb22a4bde7e9f15d6385589316b6209f1
Robert Massaioli <robertmassaioli@gmail.com>
2010-12-31 10:16:13 +1100

    Slowly learning how to write these command line arguments.

--- src/Main.hs
+++ src/Main.hs
@@ -5,19 +5,41 @@ import Control.Monad
 data TodoCommand = 
             Init 
             | Add 
+               { fromFile :: Maybe FilePath 
+               , priority :: Maybe Integer
+               }
             | Done 
             | Edit { editIds :: [Integer] }
             | Help
             deriving(Eq, Show, Data, Typeable)
 
-initMode = Init &= help "Init the todo."
-addMode = Add &= help "Add one or more todo items."
-doneMode = Done &= help "Mark one or more todo items as done."
+initMode :: TodoCommand
+initMode = Init 
+            &= help "Init the todo."
+
+addMode :: TodoCommand
+addMode = Add 
+            { fromFile = Nothing &= explicit 
+                                 &= name "from-file" 
+                                 &= help "Add a bunch of todo items from a file."
+            , priority = Nothing &= name "priority" 
+                                 &= help "What priority level is this todo item." 
+            }
+            &= help "Add one or more todo items."
+
+doneMode :: TodoCommand
+doneMode = Done 
+            &= help "Mark one or more todo items as done."
+
+editMode :: TodoCommand 
 editMode = Edit {
                editIds = [] &= args
             }
             &= help "Edit one or more todo items."
-helpMode = Help &= help "Show global help for htodo."
+
+helpMode :: TodoCommand 
+helpMode = Help 
+            &= help "Show global help for htodo."
 
 combinedModes :: TodoCommand
 combinedModes = modes 
