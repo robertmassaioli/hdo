90a03e381ce7991b5ec7911d52c0f41650f3e00f
Robert Massaioli <robertmassaioli@gmail.com>
2011-01-03 21:56:10 +1100

    Added in a whole lot more crud to the Main file.
    It is in an attempt to see what the best way to do this is. It is proving to be pretty difficult
    since it seems like I almost want to write my own ORM in Haskell which could be quite fun. :)

--- src/Main.hs
+++ src/Main.hs
@@ -1,36 +1,98 @@
-import Control.Monad (unless)
+import Control.Monad (unless, liftM)
+import Control.Monad.Reader
 import Data.Maybe (catMaybes)
 import Data.List (intersperse)
 
 import Database.HDBC
 import Database.HDBC.Sqlite3
 import Data.Time (LocalTime)
+import Data.List(sortBy)
 
 import Filter
 import TodoArguments
 
+import Text.Parsec
+import Text.Parsec.String
+import Text.Parsec.Token
+
+import Text.Show.Pretty
+
+import System.Directory
+import System.FilePath
+
+prettyShow :: (Show a) => a -> IO ()
+prettyShow = putStrLn . ppShow
+
+data Config = Config 
+   { defaultDatabaseName :: String
+   , defaultAppDirectory :: FilePath
+   , defaultSchemaDir :: FilePath
+   }
+   deriving(Show)
+
+defaultConfig :: IO Config
+defaultConfig = do 
+   appDir <- getAppUserDataDirectory "htodo"
+   return $ Config
+      { defaultDatabaseName = "htodo.db"
+      , defaultAppDirectory = appDir
+      , defaultSchemaDir = "./schema"
+      }
+
 main = do
+   config <- defaultConfig
+   runReaderT setupAppDir config
+   prettyShow config
    command <- getCommandInput
-   print command
+   prettyShow command
    executeCommand command
 
+setupAppDir :: ReaderT Config IO ()
+setupAppDir = do
+   config <- ask
+   liftIO $ do
+      appDirExists <- doesDirectoryExist (defaultAppDirectory config)
+      unless appDirExists $ do
+         createDirectory (defaultAppDirectory config)
+         createDatabase 
+            ((defaultAppDirectory config) </> (defaultDatabaseName config)) 
+            (defaultSchemaDir config </> create_file)
+   where
+      create_file :: FilePath
+      create_file = "create_database.sqlite3.read"
+
+createDatabase :: FilePath -> FilePath -> IO ()
+createDatabase databaseFile schemaFile = do
+   conn <- connectSqlite3 databaseFile
+   putStrLn databaseFile
+   createCommands <- readFile schemaFile
+   prettyShow $ map init . lines $ createCommands
+   withTransaction conn (createEverything createCommands)
+   disconnect conn
+   where
+      createEverything :: (IConnection conn) => String -> conn -> IO ()
+      createEverything createCommands conn = mapM_ (quickQuery'' conn []) . map init . lines $ createCommands
+         where quickQuery'' conn qry reps = quickQuery' conn reps qry 
+
 executeCommand :: TodoCommand -> IO ()
-executeCommand x@(Show {}) = executeShowCommand x
+executeCommand x@(Show {}) = runReaderT executeShowCommand x
 executeCommand x@(Init {}) = executeInitCommand x
 executeCommand x@(Add {}) = executeAddCommand x
 executeCommand x@(Edit {}) = executeEditCommand x
 executeCommand x@(Done {}) = executeDoneCommand x
 
-executeShowCommand :: TodoCommand -> IO ()
-executeShowCommand (Show sa sd f_one f_two) = do
-   --print filter_str
-   unless (filter_str == "") $ print (getFilters filter_str)
-   conn <- connectSqlite3 ".htodo.db"
-   getTodoItems conn >>= displayItems
-   disconnect conn
-
+executeShowCommand :: ReaderT TodoCommand IO ()
+executeShowCommand = do
+   showFlags <- ask
+   case showUsingTags showFlags of
+      Nothing -> liftIO $ putStrLn "No Tags."
+      Just x -> liftIO $ print $ separateCommas x
+   liftIO $ unless (filter_str showFlags == "") $ print (getFilters $ filter_str showFlags)
+   conn <- liftIO $ connectSqlite3 ".htodo.db"
+   liftIO $ getTodoItems conn >>= displayItems
+   liftIO $ disconnect conn
    where
-      filter_str = concat . intersperse "," . catMaybes $ [f_one, f_two]
+      filter_str showFlags = concat . intersperse "," . catMaybes $ [showUsingFilter showFlags, showFilterExtra showFlags]
 
 displayItems :: [Item] -> IO ()
 displayItems = mapM_ (displayItemHelper 0)
@@ -45,7 +107,7 @@ displayItems = mapM_ (displayItemHelper 0)
 getTodoItems :: (IConnection c) => c -> IO [Item]
 getTodoItems conn = do
    topLevels <- quickQuery' conn "select i.* from items i where i.parent_id is ?" [SqlNull]
-   mapM createChild topLevels
+   mapM createChild topLevels >>= return . sortItems
    where
       createChild :: [SqlValue] -> IO Item
       createChild [iid, ide, ica, _, ipr] = do
@@ -56,8 +118,11 @@ getTodoItems conn = do
                   , itemDescription = fromSql ide
                   , itemCreatedAt = fromSql ica
                   , itemPriority = fromSql ipr
-                  , itemChildren = children
+                  , itemChildren = sortItems children
                   }
+
+      sortItems :: [Item] -> [Item]
+      sortItems = sortBy $ \x y -> compare (itemPriority x) (itemPriority y)
 
 data Item = Item
    { itemId :: Integer
@@ -81,3 +146,14 @@ executeDoneCommand :: TodoCommand -> IO ()
 executeDoneCommand _ = unimplemented
 
 unimplemented = putStrLn "Not Implemented Yet"
+
+separateBy :: Char -> String -> Maybe [String]
+separateBy sep input = case parse parseCommas "(unknown)" input of
+   Left _ -> Nothing
+   Right x -> Just x
+   where
+      parseCommas :: Parser [String]
+      parseCommas = sepBy (many1 (noneOf [sep])) (char sep)
+
+separateCommas :: String -> Maybe [String]
+separateCommas = separateBy ','
--- src/TodoArguments.hs
+++ src/TodoArguments.hs
@@ -12,18 +12,26 @@ data TodoCommand
                { showEntireHierarchy :: Bool
                , showDone :: Bool
                , showUsingFilter :: Maybe String
+               , showUsingTags :: Maybe String
                , showFilterExtra :: Maybe String
+               , databaseFile :: Maybe FilePath
                }
             | Init 
                { userLevel :: Bool 
+               , databaseFile :: Maybe FilePath
                }
             | Add 
                { fromFile :: Maybe FilePath 
                , priority :: Maybe Integer
+               , databaseFile :: Maybe FilePath
                }
             | Done 
-            | Edit { editIds :: [Integer] }
-            | Help
+               { databaseFile :: Maybe FilePath
+               }
+            | Edit 
+               { editIds :: [Integer] 
+               , databaseFile :: Maybe FilePath
+               }
             deriving(Eq, Show, Data, Typeable)
 
 getCommandInput :: IO TodoCommand
@@ -48,25 +56,37 @@ combinedModes = modes
 
 showMode :: TodoCommand
 showMode = Show 
-               { showEntireHierarchy = def 
-                  &= explicit
-                  &= name "a" &= name "all"
-                  &= help "Show the entire hierarchy of the todo list items."
-               , showDone = def
-                  &= explicit
-                  &= name "d" &= name "done"
-                  &= help "Also show the items that are done."
-               , showUsingFilter = def
-                  &= explicit
-                  &= name "filter"
-                  &= typ "f1,f2,f3,..."
-                  &= help ("Sometimes the normal command line options are not enough; this allows you to "
-                     ++ "specify a filter. The default is obviously to filter on nothing and thus show "
-                     ++ "everything.")
-               , showFilterExtra = def
-                  &= args
-                  &= typ "filters..."
-               }
+            { showEntireHierarchy = def 
+               &= explicit
+               &= name "a" &= name "all"
+               &= help "Show the entire hierarchy of the todo list items."
+            , showDone = def
+               &= explicit
+               &= name "done"
+               &= help "Also show the items that are done."
+            , showUsingFilter = def
+               &= explicit
+               &= name "filter"
+               &= typ "f1,f2,f3,..."
+               &= help ("Sometimes the normal command line options are not enough; this allows you to "
+                  ++ "specify a filter. The default is obviously to filter on nothing and thus show "
+                  ++ "everything.")
+            , showUsingTags = def
+               &= explicit
+               &= name "t" &= name "tags"
+               &= typ "tag1,tag2,tag3,..."
+               &= help ("Only data that are tagged with the tags that you supply will be included. If "
+                  ++ "no tag was provided when the todo item was added then it will be tagged 'main' by "
+                  ++ "default")
+            , showFilterExtra = def
+               &= args
+               &= typ "filters..."
+            , databaseFile = def 
+               &= explicit
+               &= name "d" &= name "database"
+               &= typ "./path/to/database.db"
+               &= help "This is the path to the database file."
+            }
             &= help "Show todo items from the database."
             &= details
             [ "You are eventually going to want to display items from the database and this is what will "
@@ -75,12 +95,17 @@ showMode = Show
 
 initMode :: TodoCommand
 initMode = Init 
-               { userLevel = False  
-                  &= explicit
-                  &= name "for-user"
-                  &= help ("Specifies wether or not this is for the user or the current directory." 
-                            ++ " Current directory is used by default.")
-               }
+            { userLevel = False  
+               &= explicit
+               &= name "for-user"
+               &= help ("Specifies wether or not this is for the user or the current directory." 
+                         ++ " Current directory is used by default.")
+            , databaseFile = def
+               &= explicit
+               &= name "d" &= name "database"
+               &= typ "./path/to/database.db"
+               &= help "This is the path to the database file."
+            }
             &= help "Initialise the todo database and config."
             &= details 
                [ "This function allows you to create a todo list database in either the current directory "
@@ -96,6 +121,11 @@ addMode = Add
             , priority = Nothing &= name "priority" 
                                  &= help "What priority level is this todo item." 
                                  &= typ "[1-9]"
+            , databaseFile = def
+               &= explicit
+               &= name "d" &= name "database"
+               &= typ "./path/to/database.db"
+               &= help "This is the path to the database file."
             }
             &= help "Add one or more todo items."
             &= details
@@ -110,6 +140,12 @@ addMode = Add
 
 doneMode :: TodoCommand
 doneMode = Done 
+            { databaseFile = def
+               &= explicit
+               &= name "d" &= name "database"
+               &= typ "./path/to/database.db"
+               &= help "This is the path to the database file."
+            }
             &= help "Mark one or more todo items as done."
             &= details
             [ "Eventually you will want to mark items as done. This allows you to mark items as done and lets "
@@ -118,8 +154,13 @@ doneMode = Done
             ]
 
 editMode :: TodoCommand 
-editMode = Edit {
-               editIds = [] &= args
+editMode = Edit 
+            { editIds = [] &= args
+            , databaseFile = def 
+               &= explicit
+               &= name "d" &= name "database"
+               &= typ "./path/to/database.db"
+               &= help "This is the path to the database file."
             }
             &= help "Edit one or more todo items."
             &= details
@@ -127,4 +168,3 @@ editMode = Edit {
             ++ "can edit the description, add/remove tags, parent/reparent todo items and otherwise do any kind "
             ++ "of editing of the todo items that you would like."
             ]
-
